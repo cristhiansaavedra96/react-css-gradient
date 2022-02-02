3e8b416aa48c8f52ee29a8b94d0bf5dfbe2007f6
Cristhian Saavedra <cristhian@versaplace.com>
2022-02-02 16:24:40 -0300

    server fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     REACT_APP_URL: "https://css-gradient.herokuapp.com/",
-    NODE_SERVER_URL: "http://localhost:4000",
+    NODE_SERVER_URL: "https://node-css-gradient.herokuapp.com",
 }
 
 export default config;
